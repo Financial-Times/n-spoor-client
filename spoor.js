9b481ce768eee954acb312a4dea335330b3cfae9
ifeanyiisitor <ifeanyiisitor@gmail.com>
2016-03-29 16:46:35 +0100

    high level spoor api creation

---
+++ spoor.js
@@ -0,0 +1,22 @@
+export default class SpoorApi {
+    
+	static submitUserCreatedEvent (context) {
+		console.log('============= SPOOR CREATED USER', context); // TODO: Remove
+	    // TODO: Implement
+	}
+	
+	static submitUserCreationFailureEvent (context) {
+		console.log('============= SPOOR FAILED USER CREATION', context); // TODO: Remove
+	    // TODO: Implement
+	}
+	
+	static submitSubscriptionCreatedEvent (context) {
+		console.log('============= SPOOR CREATED SUBSCRIPTION', context); // TODO: Remove
+	    // TODO: Implement
+	}
+	
+	static submitSubscriptionCreationFailureEvent (context) {
+		console.log('============= SPOOR SUBSCRIPTION CREATION FAILURE', context); // TODO: Remove
+	    // TODO: Implement
+	}
+}
