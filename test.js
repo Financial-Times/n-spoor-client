aabbbe24c9a8c293ac851d0a7d47ebdd6a4560e6
Matt Brennan <mattyb1000@gmail.com>
2016-06-16 16:05:54 +0100

    fix tests

--- test.js
+++ test.js
@@ -14,6 +14,25 @@ describe('Spoor client', () => {
 
 	afterEach(() => nock.cleanAll());
 
+	it('should send an event to Spoor', () => {
+		const scope = nock('https://spoor-api.ft.com/')
+		.post('/ingest')
+		.reply(202, {});
+
+		const client = new SpoorClient({
+			source: 'spoor-client',
+			category: 'test',
+			req: fakeRequest({}),
+		});
+
+		return client.submit({
+			action: 'test',
+			context: {},
+		}).then(() => {
+			console.assert(scope.isDone(), 'should have sent event');
+		});
+	});
+
 	it('should send an event to Spoor using cookie header from Varnish', () => {
 		const scope = nock('https://spoor-api.ft.com/', {
 			reqheaders: {
